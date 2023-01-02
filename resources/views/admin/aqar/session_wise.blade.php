66fc2cfce0b929f61dff77650f8dff4c613bbee5
JagriteeSrivastava <jagriteesrivastava888@gmail.com>
2023-01-02 23:11:30 +0530

    frontend dynamic AQAR

--- resources/views/admin/aqar/session_wise.blade.php
+++ resources/views/admin/aqar/session_wise.blade.php
@@ -68,7 +68,7 @@
         <div class="col-lg-12">
             <div class="card">
                 <div class="card-header align-items-center d-flex">
-                    <h4 class="card-title mb-0 flex-grow-1">Manage Roles</h4>
+                    <h4 class="card-title mb-0 flex-grow-1">Manage AQAR</h4>
                 </div><!-- end card header -->
                 <div class="card-body">
                     <table class="table table-nowrap container">
