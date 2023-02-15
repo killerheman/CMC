48bacbe5d2cf1f389f577e9c08fffe8b1fd41b4f
KillerHeman <sharmahcool5@gmail.com>
2023-02-15 23:26:29 +0530

    first

--- resources/views/home/department/faculty_commerce.blade.php
+++ resources/views/home/department/faculty_commerce.blade.php
@@ -15,23 +15,226 @@
 
                 <div class="team-wrapper col-md-10 col-sm-7">
                     <h2 class="no-margin-top has-divider text-highlight">Faculty of Commerce</h2>
-                    <h3 class="no-margin-top has-divider text-highlight">Demo Department</h3>
+                    {{-- <h3 class="no-margin-top has-divider text-highlight">Department</h3> --}}
 
                     <div class="row page-row">
-                        <figure class="thumb col-md-3 col-sm-4 col-xs-6">
-                            <img class="img-responsive"
-                                src='{{ asset('frontend/assets/images/icon/female.png')}}'
-                                alt="" />
-                        </figure>
-                        <div class="details col-md-9 col-sm-8 col-xs-6">
-                            <h3 class="title">
-                                Demo Teacher</h3>
-                            <h4>
-                                Associate Professor</h4>
-                            <p>
-                                M.A., Ph.D.
-                            </p>
+                        <div class="col-sm-4">
+                            <figure class="thumb">
+                                <img class="img-responsive"
+                                    src='{{ asset('frontend/assets/images/icon/male.png')}}'
+                                    alt="" />
+                            </figure>
+                            <div class="details">
+                                <h3 class="title text-center">
+                                    Dr. D. P. Gupta</h3>
+                                {{-- <h4>
+                                    Associate Professor</h4>
+                                <p>
+                                    M.A., Ph.D.
+                                </p> --}}
+                            </div>
                         </div>
+
+                        <div class="col-sm-4">
+                            <figure class="thumb">
+                                <img class="img-responsive"
+                                    src='{{ asset('frontend/assets/images/icon/male.png')}}'
+                                    alt="" />
+                            </figure>
+                            <div class="details">
+                                <h3 class="title text-center">
+                                    Dr. A. K. Poddar</h3>
+                                {{-- <h4>
+                                    Associate Professor</h4>
+                                <p>
+                                    M.A., Ph.D.
+                                </p> --}}
+                            </div>
+                        </div>
+
+                        <div class="col-sm-4">
+                            <figure class="thumb">
+                                <img class="img-responsive"
+                                    src='{{ asset('frontend/assets/images/icon/male.png')}}'
+                                    alt="" />
+                            </figure>
+                            <div class="details">
+                                <h3 class="title text-center">
+                                    Dr. B. Sahu</h3>
+                                {{-- <h4>
+                                    Associate Professor</h4>
+                                <p>
+                                    M.A., Ph.D.
+                                </p> --}}
+                            </div>
+                        </div>
+
+
+                    </div>
+
+                    <div class="row page-row">
+                        <div class="col-sm-4">
+                            <figure class="thumb">
+                                <img class="img-responsive"
+                                    src='{{ asset('frontend/assets/images/icon/female.png')}}'
+                                    alt="" />
+                            </figure>
+                            <div class="details">
+                                <h3 class="title text-center">
+                                    Dr. Divya Sharma</h3>
+                                {{-- <h4>
+                                    Associate Professor</h4>
+                                <p>
+                                    M.A., Ph.D.
+                                </p> --}}
+                            </div>
+                        </div>
+
+                        <div class="col-sm-4">
+                            <figure class="thumb">
+                                <img class="img-responsive"
+                                    src='{{ asset('frontend/assets/images/icon/male.png')}}'
+                                    alt="" />
+                            </figure>
+                            <div class="details">
+                                <h3 class="title text-center">
+                                    Prof. Lalit Sharma</h3>
+                                {{-- <h4>
+                                    Associate Professor</h4>
+                                <p>
+                                    M.A., Ph.D.
+                                </p> --}}
+                            </div>
+                        </div>
+
+                        <div class="col-sm-4">
+                            <figure class="thumb">
+                                <img class="img-responsive"
+                                    src='{{ asset('frontend/assets/images/icon/female.png')}}'
+                                    alt="" />
+                            </figure>
+                            <div class="details">
+                                <h3 class="title text-center">
+                                    Prof. Ritika Maurya</h3>
+                                {{-- <h4>
+                                    Associate Professor</h4>
+                                <p>
+                                    M.A., Ph.D.
+                                </p> --}}
+                            </div>
+                        </div>
+
+
+                    </div>
+
+                    <div class="row page-row">
+                        <div class="col-sm-4">
+                            <figure class="thumb">
+                                <img class="img-responsive"
+                                    src='{{ asset('frontend/assets/images/icon/female.png')}}'
+                                    alt="" />
+                            </figure>
+                            <div class="details">
+                                <h3 class="title text-center">
+                                    Prof. Meenu Kumari</h3>
+                                {{-- <h4>
+                                    Associate Professor</h4>
+                                <p>
+                                    M.A., Ph.D.
+                                </p> --}}
+                            </div>
+                        </div>
+
+                        <div class="col-sm-4">
+                            <figure class="thumb">
+                                <img class="img-responsive"
+                                    src='{{ asset('frontend/assets/images/icon/male.png')}}'
+                                    alt="" />
+                            </figure>
+                            <div class="details">
+                                <h3 class="title text-center">
+                                    Dr. Alok Kumar (Guest Faculty)</h3>
+                                {{-- <h4>
+                                    Associate Professor</h4>
+                                <p>
+                                    M.A., Ph.D.
+                                </p> --}}
+                            </div>
+                        </div>
+
+                        <div class="col-sm-4">
+                            <figure class="thumb">
+                                <img class="img-responsive"
+                                    src='{{ asset('frontend/assets/images/icon/male.png')}}'
+                                    alt="" />
+                            </figure>
+                            <div class="details">
+                                <h3 class="title text-center">
+                                    Dr. Md. Shakir Alam (Guest Faculty)</h3>
+                                {{-- <h4>
+                                    Associate Professor</h4>
+                                <p>
+                                    M.A., Ph.D.
+                                </p> --}}
+                            </div>
+                        </div>
+
+
+                    </div>
+
+                    <div class="row page-row">
+                        <div class="col-sm-4">
+                            <figure class="thumb">
+                                <img class="img-responsive"
+                                    src='{{ asset('frontend/assets/images/icon/male.png')}}'
+                                    alt="" />
+                            </figure>
+                            <div class="details">
+                                <h3 class="title text-center">
+                                    Dr. Ajit Kumar (Guest Faculty)</h3>
+                                {{-- <h4>
+                                    Associate Professor</h4>
+                                <p>
+                                    M.A., Ph.D.
+                                </p> --}}
+                            </div>
+                        </div>
+
+                        <div class="col-sm-4">
+                            <figure class="thumb">
+                                <img class="img-responsive"
+                                    src='{{ asset('frontend/assets/images/icon/female.png')}}'
+                                    alt="" />
+                            </figure>
+                            <div class="details">
+                                <h3 class="title text-center">
+                                    Dr. Shiba Shabbir (Guest Faculty)</h3>
+                                {{-- <h4>
+                                    Associate Professor</h4>
+                                <p>
+                                    M.A., Ph.D.
+                                </p> --}}
+                            </div>
+                        </div>
+
+                        <div class="col-sm-4">
+                            <figure class="thumb">
+                                <img class="img-responsive"
+                                    src='{{ asset('frontend/assets/images/icon/male.png')}}'
+                                    alt="" />
+                            </figure>
+                            <div class="details">
+                                <h3 class="title text-center">
+                                    Dr. Sujeet Kumar Sharma (Guest Faculty)</h3>
+                                {{-- <h4>
+                                    Associate Professor</h4>
+                                <p>
+                                    M.A., Ph.D.
+                                </p> --}}
+                            </div>
+                        </div>
+
+
                     </div>
                 </div>
                 <!--//page-content-->
